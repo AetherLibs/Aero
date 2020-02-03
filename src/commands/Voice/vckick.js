da89a5afe0017362007c88e73806546436e4b727
SoulHarsh007 <57407752+SoulHarsh007@users.noreply.github.com>
2020-02-03 23:43:08 +0530

    VCKICK Command Updated!

--- src/commands/Voice/vckick.js
+++ src/commands/Voice/vckick.js
@@ -11,18 +11,15 @@ module.exports = class extends Command {
       usage: '<user:username>'
     });
   }
-  async run(msg, [user = msg.mentions.members.first() || msg.guild.members.get(msg.content.split(" ").slice(1))]) {
+  async run(msg, [user]) {
     if (!msg.member.hasPermission("MOVE_MEMBERS"))
       return msg.responder.error(msg.language.get('COMMAND_VOICEKICK_NOPERMS'));
-    if (!user)
-      return msg.responder.error(msg.language.get('COMMAND_VOICEKICK_NOPERMS', user.length > 1));
     if (!user.voiceChannel)
       msg.responder.error(msg.language.get('COMMAND_VOICEKICK_NOVOICE'));
     user.setVoiceChannel(null);
     const embed = new MessageEmbed()
-      .setColor("0xc8da00")
       .setDescription(msg.language.get('COMMAND_VOICEKICK_VOICEKICKED'))
       .setFooter(msg.language.get('COMMAND_VOICEKICK_MODERATOR', msg.author.tag), msg.author.avatarURL());
-    msg.channel.send(embed).catch(() => null);;
+    msg.channel.send(embed).catch(() => null);
   }
-};
+}
