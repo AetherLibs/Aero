a73d3e3b649fb9b6efda08378ca6d197eb118f77
Raven <marinus@reimer.world>
2020-02-12 00:51:47 +0100

    update license year & add sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config({
 	path: process.env.NODE_ENV === 'production' ? '.env' : 'dev.env'
 });
+const Sentry = require('@sentry/node');
 
+Sentry.init({
+	dsn: process.env.SENTRY_TOKEN,
+	release: `aero@${npm_package_gitHead}`
+});
 const Manager = require('../lib/Manager');
 
 new Manager().init();
