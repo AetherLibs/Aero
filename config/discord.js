898ddf27dba37b872a588a9399b7fbae96c5809c
Ravy <ravy@farfrom.earth>
2021-01-01 02:11:46 +0100

    new api compatibility

--- config/discord.js
+++ config/discord.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-bitwise */
+/* eslint-disable no-bitwise, no-unused-vars */
 const { GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, GUILD_VOICE_STATES } = require('../lib/util/constants').intents;
 
 module.exports = {
-	disableEveryone: true,
+	allowedMentions: { parse: ['users'] },
 	fetchAllMembers: false,
 	partials: ['REACTION', 'MESSAGE', 'CHANNEL'],
 	ws: {
