695a31cf35aee203f38239a75e0941fcef23b458
Ravy <ravy@farfrom.earth>
2021-07-02 22:42:54 +0200

    update klasa for postMonitors, add anti crossposting

--- src/monitors/anti-crossposting.js
+++ src/monitors/anti-crossposting.js
@@ -15,13 +15,10 @@ module.exports = class extends Monitor {
 	async run(msg) {
 		if (!msg.guild || !msg.guild.settings.get('mod.anti.crossposting') || msg.exempt) return;
 
-		if (msg.content === msg.member.lastMessageContent && msg.channel.id !== msg.member.lastChannelID) {
-			const crossposted = await msg.guild.channels.cache.get(msg.member.lastChannelID).messages.fetch(msg.member.lastMessageID);
+		if (msg.content === msg.member.prevMessageContent && msg.channel.id !== msg.member.prevChannelID) {
+			const crossposted = await msg.guild.channels.cache.get(msg.member.prevChannelID).messages.fetch(msg.member.prevMessageID);
 			if (crossposted.deletable) crossposted.delete();
 		}
-
-		msg.member.lastChannelID = msg.channel.id;
-		msg.member.lastMessageID = msg.id;
 	}
 
 };
