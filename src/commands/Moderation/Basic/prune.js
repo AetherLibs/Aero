5f2b756db8c64312d783f2c503d55f135fb3d3f6
Ravy <ravy@farfrom.earth>
2021-09-16 18:40:13 +0200

    feat: don't prune pinned messages

--- src/commands/Moderation/Basic/prune.js
+++ src/commands/Moderation/Basic/prune.js
@@ -19,10 +19,11 @@ module.exports = class extends Command {
 
 	async run(msg, [limit = 1, filter = null]) {
 		let messages = await msg.channel.messages.fetch({ limit: 100 });
+		messages = messages.filter(mes => !mes.pinned);
 		if (filter) {
 			const user = typeof filter !== 'string' ? filter : null;
 			const type = typeof filter === 'string' ? filter : 'user';
-			messages = messages.filter(this.getFilter(msg, type, user)).filter(mes => !mes.pinned);
+			messages = messages.filter(this.getFilter(msg, type, user));
 		}
 		if (messages.has(msg.id)) limit++;
 		messages = messages.keyArray().slice(0, limit);
